b71e53c39ffe20d806d585b70331ba99139f811d
Steve Tooke <tooky@cucumber.io>
2017-10-10 14:52:16 +0100

    Check the test repo is created correctly

--- src/test/java/pro/cucumber/RevisionProviderContract.java
+++ src/test/java/pro/cucumber/RevisionProviderContract.java
@@ -2,13 +2,15 @@ package pro.cucumber;
 
 import org.junit.Before;
 import org.junit.Test;
-import pro.cucumber.gitcli.GitCliRevisionProvider;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Pattern;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public abstract class RevisionProviderContract {
@@ -23,6 +25,8 @@ public abstract class RevisionProviderContract {
         Exec.cmd("git init", rootPath);
         Exec.cmd("git add -A", rootPath);
         Exec.cmd("git commit -am \"files\"", rootPath);
+        List<String> status = Exec.cmd("git status --porcelain", rootPath);
+        assertEquals(new ArrayList<String>(), status);
     }
 
     @Test
@@ -30,7 +34,7 @@ public abstract class RevisionProviderContract {
         String sha1Pattern = "^[a-f0-9]{40}$";
         RevisionProvider revisionProvider = makeRevisionProvider(rootPath);
         String rev = revisionProvider.getRev();
-        assertTrue("Expected a sha1, got: "+rev, Pattern.matches(sha1Pattern, rev));
+        assertTrue("Expected a sha1, got: " + rev, Pattern.matches(sha1Pattern, rev));
     }
 
     protected abstract RevisionProvider makeRevisionProvider(Path rootPath);
