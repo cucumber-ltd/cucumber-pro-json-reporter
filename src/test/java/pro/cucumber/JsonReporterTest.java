e9aff709f72c0a82ee7a02c7b658459027015a10
Steve Tooke <tooky@cucumber.io>
2017-10-10 13:40:51 +0100

    Move test to correct test file

--- src/test/java/pro/cucumber/JsonReporterTest.java
+++ src/test/java/pro/cucumber/JsonReporterTest.java
@@ -1,19 +1,5 @@
 package pro.cucumber;
 
-import org.junit.Test;
-
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URISyntaxException;
-
-import static org.junit.Assert.*;
-
 public class JsonReporterTest {
-    @Test
-    public void addRevisionToUrl() throws MalformedURLException, URISyntaxException {
-        String urlString = "https://example.com/tests/results";
-        String revision = "abcd1234";
-        URI url = HTTPPublisher.createResultsUri(urlString, revision);
-        assertEquals(url.toString(), "https://example.com/tests/results/abcd1234");
-    }
+
 }
