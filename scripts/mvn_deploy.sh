08f87d2b79952dc655f627a22638ce20e7379203
Steve Tooke <tooky@cucumber.io>
2018-12-13 12:04:16 +0000

    Set GPG_TTY

--- scripts/mvn_deploy.sh
+++ scripts/mvn_deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -euf -o pipefail
 gpg -q --batch --fast-import scripts/codesigning.asc
-mvn deploy -Psign-source-javadoc --settings scripts/ci-settings.xml -DskipTests=true
+GPG_TTY=$(tty) mvn deploy -Psign-source-javadoc --settings scripts/ci-settings.xml -DskipTests=true
