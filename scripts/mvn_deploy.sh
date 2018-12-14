79f1e282ff01b7c5500465408c70ebc8b9a6287b
Steve Tooke <tooky@cucumber.io>
2018-12-14 12:24:33 +0000

    Prevent gpg asking for passphrase

--- scripts/mvn_deploy.sh
+++ scripts/mvn_deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -euf -o pipefail
 gpg -q --batch --fast-import scripts/codesigning.asc
-GPG_TTY=$(tty) mvn deploy -Psign-source-javadoc --settings scripts/ci-settings.xml -DskipTests=true
+mvn deploy -Psign-source-javadoc --settings scripts/ci-settings.xml -DskipTests=true
